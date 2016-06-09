af3bab98ab4078ed553834b44e7f1d7f1303ffe0
FALSEYING <mrfalse.plugdj@gmail.com>
2016-06-09 09:58:43 +0200

    Update system.js

--- system.js
+++ system.js
@@ -1,6 +1,11 @@
 $('#woot').click();
-API.chatLog('AUTOWOOT ONLINE!');
-function advance(obj){
 
+API.sendChat("/cap 1");
+API.chatLog('AUTOWOOT <font color=darkred>ONLINE</font>!');
+
+API.on(API.CHAT, chat);
+API.on(API.ADVANCE, advance);
+
+function advance(obj){
 $('#woot').click();
 }
