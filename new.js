7d65d326a2f10f9b1c3f2f408c9267315cbd957c
FALSEYING <mrfalse.plugdj@gmail.com>
2016-08-01 11:06:49 +0200

    Create new.js

---
+++ new.js
@@ -0,0 +1,11 @@
+// FALSEYING 2016 | Všetky práva vyhradené!
+
+API.chatLog("" + system.author + " 2016 | " + system.community + "");
+API.sendChat("" + system.botname + " is online! Version " + system.version "!");
+
+var system = {
+author: "FALSEYING",
+botname: "MFEBOT",
+version: "1.0",
+community: "MUSIC FOR EVERYONE"
+}
